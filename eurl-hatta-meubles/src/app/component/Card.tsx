10e2a8a658ce63ec98a0ed05ee7de885d8f1d486
The honored one <114240821+mokhttar@users.noreply.github.com>
2024-07-16 19:31:30 +0100

    create validation for the sign up form  + add some toasts

--- eurl-hatta-meubles/src/app/component/Card.tsx
+++ eurl-hatta-meubles/src/app/component/Card.tsx
@@ -12,11 +12,8 @@ function Card() {
     <div className="flex items-center justify-center">
       <div className="grid gap-10  xl:py-20 py-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         <CardContainer className="inter-var">
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border  ">
-            <CardItem
-              translateZ="50"
-              className="text-xl font-bold text-neutral-600 dark:text-white"
-            >
+          <CardBody className="bg-black relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border  ">
+            <CardItem translateZ="50" className="text-xl font-bold text-white">
               Créez votre design de cuisine de rêve
             </CardItem>
             <CardItem
@@ -85,11 +82,8 @@ function Card() {
         </CardContainer>
 
         <CardContainer className="inter-var ">
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border  ">
-            <CardItem
-              translateZ="50"
-              className="text-xl font-bold text-neutral-600 dark:text-white"
-            >
+          <CardBody className="bg-black relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border  ">
+            <CardItem translateZ="50" className="text-xl font-bold text-white">
               Plus de 10 ans d'expertise
             </CardItem>
             <CardItem
--- eurl-hatta-meubles/src/app/component/LoginForm.tsx
+++ eurl-hatta-meubles/src/app/component/LoginForm.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { FormEvent, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Logo from "/public/assets/Hatta Meubles.svg";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
+import { wilayas } from "@/app/Data/Wilayas";
 import {
   Select,
   SelectContent,
@@ -11,30 +12,138 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useToast } from "@/components/ui/use-toast";
 
 function LoginForm() {
-  const [value, setValue] = useState();
-  const handleChange = (event: any) => {
-    event.preventDefault();
+  // Type for wilaya
+  type Wilaya = {
+    id: number;
+    name: string;
   };
+
+  // States for form fields and errors
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [wilaya, setWilaya] = useState<string>("DEFAULT");
+  const [errors, setErrors] = useState<{ field: string; message: string }[]>(
+    []
+  ); // Array of errors
+
+  // Use toast for displaying notifications
+  const { toast } = useToast();
+
+  // Regular expression for email validation
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  // Function to handle phone number change
+  const handlePhoneNumber = (value: any) => {
+    setPhoneNumber(value);
+  };
+
+  // Function to handle first name change
+  const handleFirstName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(event.target.value);
+  };
+
+  // Function to handle last name change
+  const handleLastName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(event.target.value);
+  };
+
+  // Function to handle wilaya change
+  const handleWilayaChange = (value: string) => {
+    setWilaya(value);
+  };
+
+  // Function to handle email change
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  // Function to handle password change
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  // Function to validate form fields
+  const validateForm = (): boolean => {
+    const newErrors: { field: string; message: string }[] = [];
+
+    if (firstName.trim() === "") {
+      newErrors.push({ field: "firstName", message: "First name is required" });
+    }
+    if (lastName.trim() === "") {
+      newErrors.push({ field: "lastName", message: "Last name is required" });
+    }
+    if (
+      phoneNumber.trim() === "" ||
+      phoneNumber.length <= 12 ||
+      phoneNumber.length > 13
+    ) {
+      newErrors.push({
+        field: "phoneNumber",
+        message: "Invalid phone number format",
+      });
+    }
+    if (wilaya.trim() === "DEFAULT") {
+      newErrors.push({ field: "wilaya", message: "Please select a wilaya" });
+    }
+    if (email.trim() === "" || !emailRegex.test(email)) {
+      newErrors.push({ field: "email", message: "Invalid email address" });
+    }
+    if (password.length < 6) {
+      newErrors.push({
+        field: "password",
+        message: "Password must be at least 6 characters long",
+      });
+    }
+
+    setErrors(newErrors);
+
+    // Display toast for each error in reverse order
+    for (let i = newErrors.length - 1; i >= 0; i--) {
+      const error = newErrors[i];
+      toast({
+        title: error.message,
+        variant: "destructive",
+      });
+    }
+
+    return newErrors.length === 0;
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValid = validateForm();
+
+    if (isValid) {
+      console.log("Form is valid, submitting...");
+    } else {
+      console.log("Form validation failed");
+    }
+  };
+
   return (
-    <div className="w-full max-w-lg mx-auto p-4 sm:p-6 lg:p-8 scale-95">
+    <div className="w-full max-w-lg mx-auto p-4 sm:p-6 lg:p-8  scale-95  xl:scale-110">
       <div className="flex  mt-5 flex-col items-center">
         <Image src={Logo} alt="WebSite-Logo" width={100} />
-        {/* <p className="text-gray-400 font-semibold  text-sm  text-center">
-          Discover exquisite kitchen designs tailored to inspire!
-        </p> */}
       </div>
       <div className="py-4 mt-5">
         <p className="text-2xl sm:text-3xl lg:text-4xl font-bold text-center">
           Create your Account
         </p>
-        <form className="py-5 flex flex-col gap-4">
+        <form className="py-5 flex flex-col gap-4" onSubmit={handleSubmit}>
           <div className="flex flex-col sm:flex-row gap-2">
             <input
               type="text"
               className="flex-1 border-[1px] py-3 sm:py-4 rounded-md focus:border-black px-5 border-black placeholder:text-black"
               placeholder="Enter your first name"
+              value={firstName}
+              onChange={handleFirstName}
             />
           </div>
 
@@ -42,25 +151,32 @@ function LoginForm() {
             type="text"
             className="flex-1 border-[1px] py-3 sm:py-4 rounded-md px-5 placeholder:text-black border-black"
             placeholder="Enter your last name"
+            value={lastName}
+            onChange={handleLastName}
           />
           <div className="">
             <PhoneInput
               className="w-full border-[1px] py-3 sm:py-4 rounded-md px-5 focus:border-black placeholder:text-black border-black"
               placeholder="Enter your phone number"
-              onChange={handleChange}
-              value={value}
+              value={phoneNumber}
+              onChange={handlePhoneNumber}
               country="DZ"
             />
           </div>
           <div className="w-full">
-            <Select>
+            <Select
+              onValueChange={(value) => handleWilayaChange(value)}
+              value={wilaya}
+            >
               <SelectTrigger className="w-full py-3 sm:py-4 px-4">
                 <SelectValue placeholder="Wilaya" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
+                {wilayas?.map((element: Wilaya) => (
+                  <SelectItem key={element.id} value={element.name}>
+                    {element.name}
+                  </SelectItem>
+                ))}
               </SelectContent>
             </Select>
           </div>
@@ -69,6 +185,8 @@ function LoginForm() {
               type="text"
               className="w-full border rounded-md px-5 py-3 sm:py-4 placeholder:text-black border-black"
               placeholder="Email"
+              value={email}
+              onChange={handleEmail}
             />
           </div>
           <div className="w-full">
@@ -76,9 +194,14 @@ function LoginForm() {
               type="password"
               className="w-full border rounded-md px-5 py-3 sm:py-4 placeholder:text-black border-black"
               placeholder="Password"
+              value={password}
+              onChange={handlePassword}
             />
           </div>
-          <button className="w-full py-3 sm:py-4 rounded-md font-semibold text-white bg-black mt-2">
+          <button
+            type="submit"
+            className="w-full py-3 sm:py-4 rounded-md font-semibold text-white bg-black mt-2"
+          >
             Create
           </button>
         </form>
